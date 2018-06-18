eb7a1d5858640491609d4169991f85a9c0cfa7a7
Matthew Pelosi <mattpelosi@gmail.com>
2018-06-18 15:22:31 -0700

    moved .env

--- server/server.js
+++ server/server.js
@@ -4,21 +4,14 @@ const bodyParser = require("body-parser");
 const routes = require("./routes");
 const mongo = require("./mongodb");
 const dotenv = require("dotenv");
-// const https = require("https");
-// const fs = require("fs");
 
-// const httpsOptions = {
-//   key: fs.readFileSync("/sf.code/helpful-human-new-clone/Helpful_Human_Interview_Challenge/server/ssl/MyKey.key"),
-//   cert: fs.readFileSync("/sf.code/helpful-human-new-clone/Helpful_Human_Interview_Challenge/server/ssl/MyCertificate.crt")
-// };
-
-dotenv.config({path:`/sf.code/helpful-human-new-clone/Helpful_Human_Interview_Challenge/.env`});
+dotenv.config({path:`/sf.code/helpful-human-new-clone/Helpful_Human_Interview_Challenge/server/.env`});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://hh-interview-challenge-mp.herokuapp.com");
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("withCredentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
@@ -33,7 +26,6 @@ app.use(routes);
 
 mongo
   .connect(process.env.MONGODB_URL)
-  // .then(https.createServer(httpsOptions, app).listen(8080))
   .then(app.listen(8080))
   .catch(err => {
     console.error(err);
