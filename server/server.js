c1b49649049ef61ed5b63d0d9a3b0785ed52847c
Matthew Pelosi <mattpelosi@gmail.com>
2018-06-18 11:40:50 -0700

    adjusted http settings_v2

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ dotenv.config({path:`/sf.code/helpful-human-new-clone/Helpful_Human_Interview_Ch
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://hh-interview-challenge-mp.herokuapp.com");
+  // res.header("Access-Control-Allow-Origin", "https://hh-interview-challenge-mp.herokuapp.com");
   // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("withCredentials", true);
@@ -34,10 +34,10 @@ app.use(routes);
 mongo
   .connect(process.env.MONGODB_URL)
   // .then(https.createServer(httpsOptions, app).listen(8080))
-  .then(app.listen(8080))
+  .then(app.listen(80))
   .catch(err => {
     console.error(err);
     process.exit(1);
   });
 
-console.log("server listening on port 8080");
+console.log("server listening on port 80");
