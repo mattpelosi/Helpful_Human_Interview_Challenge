fc58bcedbf766f8ce591ba3e6697464283820731
Matthew Pelosi <mattpelosi@gmail.com>
2018-07-02 17:44:11 -0700

    removed unecessary code

--- server/routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-const colorController = require("./color.controller")
-
-module.exports = router
-
-router.get("/scrape-colors", colorController.colorScraper);
-router.get("/colors", colorController.read);
